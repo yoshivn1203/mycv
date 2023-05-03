f99fde4aec61a79c884332e72febab3fb4ad5a9d
yoshivn1203 <yoshivn1203@gmail.com>
2023-05-03 14:14:07 +0700

    finish section 9

---
+++ src/users/user.entity.ts
@@ -0,0 +1,35 @@
+import {
+  AfterInsert,
+  AfterUpdate,
+  AfterRemove,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Update User with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Remove User with id', this.id);
+  }
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { createUserDto } from './dtos/create-user.dto';
+import { updateUserDto } from './dtos/update-user.dto';
+
+@Controller('auth')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: createUserDto) {
+    this.userService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.userService.findOne(parseInt(id));
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: updateUserDto) {
+    return this.userService.update(parseInt(id), body);
+  }
+}
